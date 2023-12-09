6c38ca0ad8e6bb9540f19be06de0f97346c32d6c
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-12-09 09:50:33 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,38 @@
+from pyrogram import Client, filters
+from config import LOG_CHANNEL
+from plugins.database import db
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    CallbackQuery,
+    Message
+)
+
+LOG = LOG_CHANNEL
+
+LOG_TEXT = """<b>#NewUser
+    
+ID - <code>{}</code>
+
+Nᴀᴍᴇ - {}</b>
+"""
+
+@Client.on_message(filters.command('start'))
+async def start_message(c,m):
+    await db.is_user_exist(m.from_user.id)
+    await db.add_user(m.from_user.id, m.from_user.first_name)
+    await c.send_message(LOG, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
+    await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
+        caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ**\n**/help ғᴏʀ ᴋɴᴏᴡ ᴍᴏʀᴇ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[Tᴇᴄʜ VJ](https://t.me/vj_botz)**",
+        reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ')
+                    ],  
+                    [
+                        InlineKeyboardButton("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/Kingvj01'),
+                        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url='https://t.me/VJ_Botz')
+                    ]
+                ]
+            )
+  
