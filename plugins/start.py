0d9fda1d2a9b782602627735a14502f398cc439f
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-12-25 05:53:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,6 @@ from pyrogram.types import (
     Message
 )
 
-LOG = LOG_CHANNEL
 
 LOG_TEXT = """<b>#NewUser
     
@@ -21,7 +20,7 @@ Nᴀᴍᴇ - {}</b>
 async def start_message(c,m):
     await db.is_user_exist(m.from_user.id)
     await db.add_user(m.from_user.id, m.from_user.first_name)
-    await c.send_message(LOG, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
+    await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
     await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
         caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[Tᴇᴄʜ VJ](https://t.me/vj_botz)**",
         reply_markup=InlineKeyboardMarkup(
